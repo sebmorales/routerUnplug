048d5837ffcc44709d8f81da5c4d2eda0b5e73e8
sebmorales <sebmoralesprado@gmail.com>
2017-11-14 17:39:30 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,142 @@
+//Express
+var express = require('express');
+var app = express();
+//HTTP and creating our awasome server
+var http = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+var server = app.listen(port);
+
+//bodyParser helps us decode the queries
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(express.json());       // to support JSON-encoded bodies
+// app.use(express.urlencoded()); // to support URL-encoded bodies
+var pcap = require('pcap'),
+    tcp_tracker = new pcap.TCPTracker(),
+    pcap_session = pcap.createSession('en0', "ip proto \\tcp");
+
+var host =require('host');
+var dns  = require('dns');
+//SerialPort in case you were connected to an arduino or a serial device
+// var SerialPort = require('serialport');
+// var serialPort = new SerialPort("/dev/cu.usbmodem1411", {
+//   baudrate: 9600,
+// 	parser: SerialPort.parsers.readline('\n')
+// });
+var bannedURLS=[];
+var willdie=-1;
+var d = new Date();
+
+
+
+class bannedIp {
+  constructor(url) {
+    this.url = url;
+    this.ips=[];
+  }
+  resolveIps(){
+    dns.resolve4(this.url,(err,addresses) => {
+      if(err) throw err;
+      var ipJSON=JSON.parse(JSON.stringify(addresses));
+      for(let i=0;i<ipJSON.length;i++){
+        this.ips.push(ipJSON[i]+":443");
+      }
+      // console.log(this.ips)
+
+    })
+  }
+}
+
+console.log("Server running and listening at port " + port);
+
+//In case the user goes to http://localhost:3000/unplug
+app.get("/unplug", function(req, res){
+  unplug();
+  res.send('Bye\n');
+});
+
+app.get("/unplug/banned", function(req, res){
+  if(bannedURLS.length<1){
+    res.send("No websites have been banned. To ban a website post the url to 'newBannedURL=yourbannedwebsite.com'")
+  }else{
+    res.send(bannedURLS);
+  }
+});
+
+
+app.get("/unplug/willdie", function(req, res){
+  if(willdie>0){
+    var date=new Date();
+    console.log(willdie+" "+date);
+    res.send("the router will unplug in "+(Number(willdie)-Number(date))+" milliseconds");
+  }else{
+    res.send("the router will live forever... for now.");
+  }
+});
+
+//In case the user sends a post request, here you can schedule the unplug
+//curl -X POST -d 'time=5000' 'http://localhost:3000/unplug'
+//curl -X POST -d 'newBannedURL=facebook.com' 'http://localhost:3000/unplug'
+
+app.post("/unplug",function(req,res){
+    var waitfor=Number(req.body.time);
+    var tempURL=(req.body.newBannedURL);
+    console.log(tempURL);
+
+    if(!(isNaN( waitfor ))){
+      console.log(waitfor);
+      setTimeout(function () {
+        unplug();
+      },waitfor);
+      var date=Number(new Date());
+      willdie=waitfor+date;
+      res.send('Will unplug in '+waitfor+' milliseconds.\nBye!\n');
+    }else if(typeof tempURL !== 'undefined'){
+      bannedURLS.push(new bannedIp(tempURL));
+      bannedURLS[bannedURLS.length-1].resolveIps();
+      res.send('watching for '+tempURL);
+    }else{
+      res.send("I couldn't get what you mean?")
+    }
+});
+
+
+function unplug(){
+  //send command to motors to unplug
+  console.log("unplugging");
+}
+
+
+
+for(let i=0;i<bannedURLS.length;i++){
+  console.log(bannedURLS[i].url);
+  bannedURLS[i].resolveIps();
+}
+
+
+tcp_tracker.on('session', function (session) {
+  var destIP=session.dst_name;
+   // console.log(bannedURLS.length)
+
+  for(let j=0;j<bannedURLS.length;j++){
+    for(let i=0;i<bannedURLS[j].ips.length;i++){
+      // console.log("here")
+      if((bannedURLS[j].ips[i])==destIP){
+        console.log("user went to "+bannedURLS[j].url);
+        unplug();
+      }
+    }
+  }
+  // console.log("Start of session between " + session.src_name + " and " + session.dst_name);
+  session.on('end', function (session) {
+      // console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
+  });
+});
+
+pcap_session.on('packet', function (raw_packet) {
+    var packet = pcap.decode.packet(raw_packet);
+    tcp_tracker.track_packet(packet);
+});
